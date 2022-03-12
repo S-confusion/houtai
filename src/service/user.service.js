6f3cc5fa682445672a2161d47fd5d79a066e1173
sunqianjin <sqj18855212511@163.com>
2022-03-12 22:53:17 +0800

    test

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,6 +1,6 @@
 class userService {
     async createUser({ userName, password }) {
-        return '我收到了用户名密码并写入了数据库!'
+        return '我收到了用户名密码并写入了数据库!///'
     }
 };
 module.exports =new userService()
